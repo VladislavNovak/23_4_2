7606b088fb1fdad9bb46857ef2ccaa84603221f4
Novak <novaldix@gmail.com>
2023-08-14 20:54:02 +0300

    23_4_2 added solution

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+using std::cout;
+using std::endl;
+
+#define SPRING 1
+#define SUMMER 2
+#define AUTUMN 3
+#define WINTER 4
+#define SELECT SUMMER
+#define PRINT_RESULT(x) (printf("This is %s", #x));
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#if SELECT == SPRING
+    PRINT_RESULT(spring)
+#endif
+#if SELECT == SUMMER
+    PRINT_RESULT(summer)
+#endif
+#if SELECT == AUTUMN
+    PRINT_RESULT(autumn)
+#endif
+#if SELECT == WINTER
+    PRINT_RESULT(winter)
+#endif
 }
